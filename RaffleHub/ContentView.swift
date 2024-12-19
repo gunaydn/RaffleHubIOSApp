1147aab22bb8c961bb7ce6d92a8a32f0585a7ac8
Kaan Cem Günaydın <gunaydnk@gmail.com>
2024-12-19 15:27:53 +0300

    Initial Commit

---
+++ RaffleHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaffleHub
+//
+//  Created by Kaan Cem Günaydın on 19.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaffleHub/RaffleHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaffleHubApp.swift
+//  RaffleHub
+//
+//  Created by Kaan Cem Günaydın on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RaffleHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
